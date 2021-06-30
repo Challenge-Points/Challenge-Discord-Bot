a2525d85f8eb0b491073e058e85d2bc141c0ec65
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-01 00:13:19 +0100

    Allowed jskp to work across guilds

--- utils/jskp.py
+++ utils/jskp.py
@@ -1,11 +1,13 @@
 from jishaku import Jishaku, Feature
-from discord.ext.commands import Context
+
+from discord.ext.commands import Context, MissingPermissions
+
 
 async def cog_check_patch(self: Feature, ctx: Context):
-    if "797422816584007720" in str(ctx.author.roles): 
+    if "812293998978138152" in str((ctx.bot.get_guild(812293813518991390).get_member(ctx.author.id)).roles):
         return True
     if ctx.author.id == ctx.bot.owner_id:
         return True
-    return False
+    raise MissingPermissions("Challenge Bot Developer")
 
 Jishaku.cog_check = cog_check_patch
