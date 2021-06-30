345ce7667d20a9c6012028d4b862112eb304e93d
Sirspam <Charlespattison555@Gmail.com>
2021-06-30 10:46:19 +0100

    Changed to use aiohttp

--- cogs/cp/stats.py
+++ cogs/cp/stats.py
@@ -1,7 +1,7 @@
-import discord
-import logging
-import json
-import requests
+from json import loads
+
+from discord import Embed
+
 from discord.ext import commands
 
 
@@ -12,16 +12,15 @@ class stats(commands.Cog):
     
     @commands.command(case_insensitive=True)
     async def stats(self, ctx):
-        logging.info(f"recieved stats request in {ctx.guild.name}")
-        print(self.bot.header)
-        json_data=json.loads(requests.get("https://challenge-points-dev.herokuapp.com/api/stats/all", headers=self.bot.header).text)
-        embed = discord.Embed(
-            title="Challenge Points Stats",
-            description="Scores set: "+str(json_data["scores_set"])+"\nMaps ranked: "+str(json_data["maps_ranked"])+"\nUsers registered: "+str(json_data["users_registerd"])+"\nIn queue: "+str(json_data["in_queue"])+"\nAPI calls today: "+str(json_data["apicalls"]),
-            colour=0xff0c10
-        )
+        async with ctx.channel.typing():
+            async with self.bot.session.get("https://challengepointsapi.herokuapp.com/api/stats/all") as resp:
+                json_data = loads(await resp.text())
+            embed = Embed(
+                title="Challenge Points Stats",
+                description="Scores set: "+str(json_data["scores_set"])+"\nMaps ranked: "+str(json_data["maps_ranked"])+"\nUsers registered: "+str(json_data["users_registerd"])+"\nIn queue: "+str(json_data["in_queue"])+"\nAPI calls today: "+str(json_data["apicalls"]),
+                colour=0xff0c10
+            )
         await ctx.send(embed=embed)
-        logging.info("Sent stats embed\n------------")
 
 
 
