494c636dd140c130c2d5c6496f768807aa1293e9
Sirspam <Charlespattison555@Gmail.com>
2021-06-30 10:46:04 +0100

    Reorganised bot code structure

--- bot.py
+++ bot.py
@@ -1,27 +1,31 @@
-import discord
-import os
 import logging
-import firebase_admin
-from firebase_admin import credentials
 from logging.handlers import RotatingFileHandler
+from asyncio import get_event_loop
+from os import getcwd, getenv
+
+from discord import Intents, AllowedMentions
+from aiohttp import ClientSession
 from dotenv import load_dotenv
-from random import randint
-from discord.ext import commands, tasks
+from firebase_admin import credentials, initialize_app
+
+from discord.ext.commands import Bot
 from utils import jskp
 
 
-cwd = os.getcwd()
-load_dotenv(f"{cwd}/config.env")
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!cb ",intents=intents,case_insensitive=True,allowed_mentions=discord.AllowedMentions(replied_user=False))
-bot.header = {"User-Agent": "Challenge Bot (https://github.com/Challenge-Points/Challenge-Discord-Bot)"}
-logging.basicConfig(format= '%(asctime)s:%(levelname)s:%(name)s: %(message)s',level=logging.INFO,handlers=[logging.handlers.RotatingFileHandler(filename="log.log", maxBytes=5000000, backupCount=1)])
+logging.basicConfig(format= '%(asctime)s:%(levelname)s:%(name)s: %(message)s',level=logging.INFO,handlers=[RotatingFileHandler(filename="log.log", maxBytes=5000000, backupCount=2)])
+
+load_dotenv(getcwd()+"/.env")
+
+intents = Intents.default()
+intents.members = True
+bot = Bot(command_prefix="!cb ", max_messages=None, intents=intents, case_insensitive=True, allowed_mentions=AllowedMentions(replied_user=False))
+
 
 cred = credentials.Certificate({
     "type": "service_account",
     "project_id": "challengepoints-475e8",
     "private_key_id": "846a1fedbb168eec61e2493cf7a354f9df1d6a5d",
-    "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),  
+    "private_key": getenv("PRIVATE_KEY").replace('\\n', '\n'),  
     "client_email": "firebase-adminsdk-vhibp@challengepoints-475e8.iam.gserviceaccount.com",
     "client_id": "117926289697887103422",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -30,27 +34,16 @@ cred = credentials.Certificate({
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-vhibp%40challengepoints-475e8.iam.gserviceaccount.com"
 })
 
-default_app = firebase_admin.initialize_app(cred)
+default_app = initialize_app(cred)
 
-status_list = [
-    "Aso kinda cute 😳",
-    "Vibros",
-    "Challenge Maps",
-    "Beat Saber",
-    "Yeet Saber",
-    "Meat Saber",
-    "Shiny Happy Days"
-]
 
 initial_cogs = [
     "jishaku",
     "cogs.cp.stats",
     "cogs.error_handler",
-    "cogs.text",
-    "cogs.link",
-    "cogs.neko"
+    "cogs.general",
+    "cogs.waifu"
 ]
-
 
 for cog in initial_cogs:
     try:
@@ -60,17 +53,18 @@ for cog in initial_cogs:
         logging.error(f"Failed to load cog {cog}: {e}")
 
 
-@tasks.loop(hours=1)
-async def status():
-    await bot.wait_until_ready()
-    value = (randint(0, len(status_list))) - 1
-    await bot.change_presence(activity=discord.Game(name=status_list[value]))
-    logging.info(f"Status set to: {status_list[value]}")
-
 @bot.event
 async def on_ready():
+    bot.session = ClientSession(loop=get_event_loop(), headers={"User-Agent": "Challenge Bot (https://github.com/Challenge-Points/Challenge-Discord-Bot)"})
     logging.info('Bot has successfully launched as {0.user}'.format(bot))
-    status.start()
+
+@bot.before_invoke
+async def before_invoke(ctx):
+    logging.info(f"Invoked {ctx.command} in {ctx.guild.name} by {ctx.author.name}\nArgs: {ctx.args}" )
+
+@bot.after_invoke
+async def after_invoke(ctx):
+    logging.info(f"Concluded {ctx.command}")
 
 
-bot.run(os.getenv("TOKEN"))
+bot.run(getenv("TOKEN"))
