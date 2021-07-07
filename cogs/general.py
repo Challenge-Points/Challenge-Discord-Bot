ab1880368f79e980c3446495e720ce0d9e85ef44
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-07 19:58:10 +0100

    Improved links command

--- cogs/general.py
+++ cogs/general.py
@@ -1,8 +1,7 @@
-import logging
-
-from discord import Embed
+from discord import Embed, Permissions
 
 from discord.ext import commands
+from discord.utils import oauth_url
 
 
 class General(commands.Cog):
@@ -17,17 +16,27 @@ class General(commands.Cog):
             await message.add_reaction("🟩")
             await message.add_reaction("🟥")
 
-    @commands.command(case_insensitive=True, aliases=["link","website"])
+    @commands.command(case_insensitive=True, aliases=["invite","website"])
     async def links(self, ctx):
-        logging.info('Recieved: links')
+        permission_names = (
+            "send_messages",
+            "embed_links",
+            "attach_files",
+            "add_reactions",
+            "use_external_emojis"
+        )
+        perms = Permissions()
+        perms.update(**dict.fromkeys(permission_names, True))
         embed = Embed(
-            description="""[Challenge Points Website](https://new-challenge-points.herokuapp.com/)
-[Challenge Points Discord](https://discord.gg/SaEgfnepn7)
-[Challenge Points GitHub](https://github.com/Challenge-Points)""",
-            color=0xff0000)
+            description=
+f"""[**Bot Invite Link**]({oauth_url(self.bot.user.id, perms)})
+[**Home Server**](https://discord.gg/SaEgfnepn7)
+[**Challenge Points Website**](https://new-challenge-points.herokuapp.com/)
+[**Github Repo**](https://github.com/Challenge-Points)\n
+I hope you're having a good day :)""",
+            colour=0xff0000)
         embed.set_thumbnail(url=(self.bot.get_guild(812293813518991390).icon_url))
-        await ctx.send(embed=embed)
-        logging.info(f'Response: embed\n----------')
+        await ctx.reply(embed=embed)
 
 
 def setup(bot):
